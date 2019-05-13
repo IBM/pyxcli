29a4287cac0eaa40bd3f527fa6e7d84fc5086f10
olgasht <olgasht@il.ibm.com>
2019-05-13 15:05:41 +0300

    CSI-pyxcli-python3: change new version number + python version support

--- setup.py
+++ setup.py
@@ -48,5 +48,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ])
